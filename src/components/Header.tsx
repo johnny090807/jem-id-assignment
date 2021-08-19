28f90ec687ebedd84f629bce5ba7f1bdd2b66660
john klees <johnnynl@live.nl>
2021-08-19 14:27:02 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 // De header
 const Header = () => {
     return (
-    <nav className="navbar sticky-top navbar-light bg-light">
+    <nav className="navbar sticky-top navbar-dark bg-success">
         <Link className="navbar-brand ml-1" to={`/`}>JEM-id</Link>
         <ul className="nav navbar-nav navbar-center">
             <li><Link to={`/products`}>Look at products</Link></li>
